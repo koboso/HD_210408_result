67e79e8fe9832b3e4b754b1b180b72e19225464d
home <HOME@HOME.COM>
2021-04-08 22:20:51 +0900

    21-04-08 정답.

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+
+
+void main() {
+
+	/*
+		*
+	   **
+	  ***
+	 ****
+	*****
+
+	1번 정답.
+
+
+	int var = 0;
+	for (int j = 0; j < 4; j++) {
+		printf("*\n");
+		for (int k = 2; k < 4+var; k++) {
+			printf("*");
+		}
+		var++;
+		printf("\n");
+
+	}
+	
+
+*
+**
+*
+***
+*
+****
+*
+*****
+	2번 정답.
+
+	/*
+	
+	for (int i = 0; i < 5; i++) {
+		for (int j = 4; j > i; j--) {
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	*/
+	fgetc(stdin);
+}
+
